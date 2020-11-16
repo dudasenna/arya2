363f0d457c836f6a6bb995d3712b0dc12fad4c37
Eduarda Senna <39406516+dudasenna@users.noreply.github.com>
2020-11-16 16:49:53 -0300

    Add files via upload

---
+++ Arya2/Arya/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Arya
+//
+//  Created by Eduarda Senna on 16/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
